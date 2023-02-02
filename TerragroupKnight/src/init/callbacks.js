fa15ed17399d3fd34962b5e0475b0acb6555bba3
Ereshkigal <ereshkigalgames@gmail.com>
2023-02-02 22:02:10 +0100

    Organization files

---
+++ TerragroupKnight/src/init/callbacks.js
@@ -0,0 +1,216 @@
+/*
+エレシュキガル
+*/
+
+/*
+    SPT-AKI Terragroup Knight mod.
+    Copyright (C) 2023  Amelia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+"use strict";
+const callbackUtils = require("../functions/callbackUtils")
+const akiFunctions = require("../functions/aki_newFunctions")
+
+class callbacksInit
+{
+    static Initializing()
+    {
+        HttpRouter.onStaticRoute["/client/game/bot/generate"]["terragroup"] = callbacksInit.raiderInvade.bind(this);
+        HttpRouter.onStaticRoute["/client/game/start"]["terragroup"] = callbacksInit.GameStartCallback.bind(this);
+        HttpRouter.onStaticRoute["/client/game/keepalive"]["terragroup"] = callbacksInit.CheckForLabsUnlock.bind(this);
+        HttpRouter.onDynamicRoute["/client/location/getLocalloot"]["terragroup"] = callbacksInit.addLoot.bind(this);
+        BotController.generateBot = akiFunctions.generateBot;
+        this.funcptr = HttpServer.onRespond["IMAGE"];
+        HttpServer.onRespond["IMAGE"] = callbacksInit.getImage.bind(this);
+        this.chance = 0;
+    }
+
+    static GameStartCallback(url, info, sessionID, output)
+    {
+        
+        const profile = SaveServer.profiles[sessionID].characters.pmc;
+        
+        //Does labs should be locked or not ?
+        callbackUtils.checkLabsLock(profile);
+
+        //Change PMC health values based on their skill
+        callbackUtils.applyHealthModifications(profile, sessionID);
+
+        //Change PMC Metabolisms valuees based on their skill
+        callbackUtils.applyMetabolismModifications(profile, sessionID);
+
+        return HttpResponse.nullResponse();
+    }
+
+    static CheckForLabsUnlock(url, info, sessionID, output)
+    {
+        const profile = ProfileController.getPmcProfile(sessionID);
+
+        callbackUtils.checkLabsLock(profile);
+
+        if(output)
+        {
+            return output
+        }  
+        else
+        {
+            return HttpResponse.getBody({
+                "msg": "OK"
+            });    
+        }
+        
+    }
+
+    static raiderInvade(url, info, sessionID)
+    {
+        const profile = ProfileController.getPmcProfile(sessionID);
+        const mapName = SaveServer.profiles[sessionID].inraid.location.toLowerCase();
+        const config = require("../../config/config.json");
+
+        if (QuestController.questStatus(profile, "twelfthQuest_terragroupSpecialist") === "Started" && mapName === "woods")
+        {
+            for (let type in info.conditions)
+            {
+                let roles = info.conditions[type];
+                roles.Role = "TG_Raiders";
+                roles.Difficulty = "impossible";
+            }
+            Logger.log("##############################################################################", "white", "red");
+            Logger.log("##############################################################################", "white", "red");
+            Logger.log("############################# YOU ARE BEING RAIDED #############################", "white", "red");
+            Logger.log("##############################################################################", "white", "red");
+            Logger.log("##############################################################################", "white", "red");
+        }
+        else if (mapName !== "laboratory")
+        {
+            if (this.chance < config.raidsChance)
+            {
+                for (let type in info.conditions)
+                {
+                    let roles = info.conditions[type];
+                    roles.Role = "TG_Raiders";
+                    roles.Difficulty = "impossible";
+                    info = info;
+                }
+
+                Logger.log("##############################################################################", "white", "red");
+                Logger.log("##############################################################################", "white", "red");
+                Logger.log("############################# YOU ARE BEING RAIDED #############################", "white", "red");
+                Logger.log("##############################################################################", "white", "red");
+                Logger.log("##############################################################################", "white", "red");
+            }
+
+
+        }
+
+            return HttpResponse.getBody(BotController.generate(info));
+        
+    }
+    static addLoot(url, info, sessionID, output)
+    {
+        this.chance = RandomUtil.getIntEx(100);
+        const value = require("../../config/values.json");
+        const profile = ProfileController.getPmcProfile(sessionID);
+        const mapName = SaveServer.profiles[sessionID].inraid.location.toLowerCase();
+
+        if (QuestController.questStatus(profile, "twelfthQuest_terragroupSpecialist") === "Started" && mapName === "woods")
+        {
+            DatabaseServer.tables.locations["woods"]["loot"]["forced"].push(value.WoodsForcedLoot);
+        }
+
+        return HttpResponse.getBody(LocationController.get(info.locationId));
+        
+    }
+
+    
+
+    static getImage(sessionID, req, resp, body)
+    {
+        const mod = require("../../package.json");
+        const filepath = `${ModLoader.getModPath(mod.name)}res/`;
+
+        if (req.url.includes("/avatar/terragroup_specialist"))
+        {
+            //Trader avatar
+            HttpServer.sendFile(resp, `${filepath}trader/terragroup_specialist.png`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/firstQuest_terragroupSpecialist.png"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/firstQuest_terragroupSpecialist.png`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/secondQuest_terragroupSpecialist.png"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/secondQuest_terragroupSpecialist.png`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/thirdQuest_terragroupSpecialist.png"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/thirdQuest_terragroupSpecialist.png`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/fourthQuest_terragroupSpecialist.png"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/fourthQuest_terragroupSpecialist.png`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/fifthQuest_terragroupSpecialist.png"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/fifthQuest_terragroupSpecialist.png`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/sixthQuest_terragroupSpecialist.jpg"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/sixthQuest_terragroupSpecialist.jpg`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/seventhQuest_terragroupSpecialist.png"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/seventhQuest_terragroupSpecialist.png`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/eightQuest_terragroupSpecialist.jpg"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/eightQuest_terragroupSpecialist.jpg`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/ninthQuest_terragroupSpecialist.png"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/ninthQuest_terragroupSpecialist.png`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/tenthQuest_terragroupSpecialist.jpg"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/tenthQuest_terragroupSpecialist.jpg`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/eleventhQuest_terragroupSpecialist.png"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/eleventhQuest_terragroupSpecialist.png`);
+            return;
+        }
+        else if (req.url.includes("/files/quest/icon/twelfthQuest_terragroupSpecialist.png"))
+        {
+            HttpServer.sendFile(resp, `${filepath}quests/twelfthQuest_terragroupSpecialist.png`);
+            return;
+        }
+
+        this.funcptr(sessionID, req, resp, body);
+    }
+}
+
+module.exports = callbacksInit;
---
+++ TerragroupKnight/src/init/database.js
@@ -0,0 +1,231 @@
+/*
+エレシュキガル
+*/
+
+/*
+    SPT-AKI Terragroup Knight mod.
+    Copyright (C) 2023  Amelia
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+"use strict";
+
+class DatabaseInit
+{
+    static createTemplates()
+    {
+        //Important constants to load
+        const CoreMod = require("../../../CoreMod/src/Core.js");
+        const log = require("../logging");
+
+        //Database constants
+        const database = DatabaseServer.tables;
+        const items = database.templates.items;
+        const suits = database.templates.customization;
+        const quests = database.templates.quests;
+        const Hideout = database.hideout;
+
+        //Terragroup database constants
+        const itemsList = require("../../db/items.json");
+        const questList = require("../../db/quests.json");
+
+        log.development("Starting initialization of the database");
+
+        log.development("Handbook initialization");
+        //Adding handbook
+        for (let i in itemsList)
+        {
+            let item = itemsList[i];
+            if (!CoreMod.DoesHandbookAlreadyExist(item._id))
+            {
+                CoreMod.CreateHandbookItem(i, item._parent, 0);
+            }
+        }
+
+        log.development("Handbook database populated");
+
+        log.development("Populating database with new items...");
+
+        //Adding new items to the game
+        for (const item in itemsList)
+        {
+            items[itemsList[item]._id] = itemsList[item];
+        }
+
+        log.development("New items added to the database");
+
+        log.development("Adding the new quests");
+        //Adding new quests to the game
+        for (const quest in questList)
+        {
+            quests[questList[quest]._id] = questList[quest];
+        }
+        log.development("New quests added");
+
+        log.development("Adding new clothing to the database");
+        const NewSuit = JsonUtil.clone(suits["5d1f56f186f7744bcb0acd1a"]);
+        const NewSuit2 = JsonUtil.clone(suits["5d1f623386f7744bcd135833"]);
+
+        //TG Top suit
+        NewSuit._id = "TG_Top";
+        NewSuit._name = "Terragroup member TOP";
+        NewSuit._props.Side = ["Savage"];
+        NewSuit._props.Prefab.path = "assets/suits/tshirt_usec_combatshirt.bundle";
+        NewSuit._props.Name = NewSuit._name;
+        NewSuit._props.ShortName = NewSuit._name;
+        NewSuit._props.Description = NewSuit._name;
+        suits[NewSuit._id] = NewSuit;
+
+        //Suits for sell(no use)
+        NewSuit2._id = "TG_Top_Suits";
+        NewSuit2._name = "Terragroup member TOP";
+        NewSuit2._props.Name = NewSuit._name;
+        NewSuit2._props.ShortName = NewSuit._name;
+        NewSuit2._props.Description = NewSuit._name;
+        NewSuit2._props.Side = NewSuit._props.Side;
+        NewSuit2._props.Body = NewSuit._id;
+        suits[NewSuit2._id] = NewSuit2;
+        log.development("All clothes added");
+
+        //Add hideout stuff
+        //Maybe it's supposed to work now ? 
+
+        
+        const eth_prod = require('../../db/hideout/production/terragroupSpecialist_ETHProduction.json');
+        const grendelProd = require('../../db/hideout/production/terragroupSpecialistGrendelProduction.json')
+        
+        Hideout.production.push(eth_prod,grendelProd)
+
+    }
+
+    static createTranslations()
+    {
+        const database = DatabaseServer.tables;
+        const locales = database.locales.global;
+        const localesList = require("../../db/locales.json");
+        for (const lang in localesList)
+        {
+            for (const node1 in localesList[lang])
+            {
+                Logger.log(localesList[lang][node1]);
+                Object.assign(locales[lang][node1], localesList[lang][node1]);
+            }
+        }
+
+    }
+
+    static applyGlobalsChanges()
+    {
+        //Database constants
+        const database = DatabaseServer.tables;
+        const global = database.globals;
+        //Terragroup database constants
+        const values = require("../../config/values.json");
+        const expTable = require("../../config/expTable.json");
+        const newPresets = require("../../config/presets.json");
+
+        //Add new weapons masteries
+        for (const mastery in values.WeaponMastery)
+        {
+            global.config.Mastering.push(values.WeaponMastery[mastery]);
+        }
+
+        //Add new weapons presets
+        for (const foo in newPresets)
+        {
+            Object.assign(global.ItemPresets, newPresets[foo]);
+        }
+
+        //Add our own globals values needed
+        global.config.exp.level.exp_table = expTable; // New EXP table to go to lvl 81
+
+        //Add new stims
+        Object.assign(global.config.Health.Effects.Stimulator.Buffs, values.BuffToAdd);
+
+
+    }
+
+    static applyMapsChanges()
+    {
+
+        //Important constants to load
+        const CoreMod = require("../../../CoreMod/src/Core.js");
+        const tgLoot = require('../../db/loot.json');
+        const cfg = require('../../config/config.json')
+
+        //Adding loot to the maps
+        for(const lootMaps in tgLoot)
+        {  
+            for(const lootCat in tgLoot[lootMaps])
+            {
+                for(const catArray in tgLoot[lootMaps][lootCat])
+                {
+                    DatabaseServer.tables.locations[lootMaps].loot[lootCat].push(tgLoot[lootMaps][lootCat][catArray]);
+                }
+            }
+        }
+
+        const newBoss = {
+            "BossName": "TG_Boss",
+            "BossChance": cfg["TG_Boss SpawnChance"],
+            "BossZone": "BotZoneFloor1,BotZoneFloor2,BotZoneBasement",
+            "BossPlayer": false,
+            "BossDifficult": "impossible",
+            "BossEscortType": "TG_Followers",
+            "BossEscortDifficult": "impossible",
+            "BossEscortAmount": "5",
+            "Time": 1,
+            "TriggerId": "",
+            "TriggerName": "none",
+            "Delay": 15,
+        };
+
+        // Adding boss to Labs
+        for (const bosses in DatabaseServer.tables.locations["laboratory"].base.BossLocationSpawn)
+        {
+            let boss = DatabaseServer.tables.locations["laboratory"].base.BossLocationSpawn[bosses];
+            boss.BossName = "TG_Raiders";
+            boss.BossEscortType = "TG_Raiders";
+            boss.BossEscortAmount = "2,3,3,5,1,1,5,2,2,3,4";
+            boss.BossDifficult = "impossible";
+            boss.BossChance = 60;
+        }
+        
+        DatabaseServer.tables.locations["laboratory"].base.BossLocationSpawn.push(newBoss);
+
+        //Lets add some spawns
+        CoreMod.AddNewSpawnPoint("Spawn1", "laboratory", -263.587, 0.01103304, -390.5421, 0, ["Terragroup"], ["Bot"], "BotZoneMain");
+        CoreMod.AddNewSpawnPoint("Spawn2", "laboratory", -245.7945, 0.02383833, -380.219, 0, ["Terragroup"], ["Bot"], "BotZoneMain");
+        CoreMod.AddNewSpawnPoint("Spawn3", "laboratory", -249.6365, 4.104155, -379.0071, 0, ["Terragroup"], ["Bot"], "BotZoneMain");
+        CoreMod.AddNewSpawnPoint("Spawn4", "laboratory", -245.6283, 4.104156, -379.9973, 0, ["Terragroup"], ["Bot"], "BotZoneMain");
+        CoreMod.AddNewSpawnPoint("Spawn5", "laboratory", -253.2756, 4.117604, -361.4047, 0, ["Terragroup"], ["Bot"], "BotZoneMain");
+        CoreMod.AddNewSpawnPoint("Spawn6", "laboratory", -255.1729, 4.117605, -367.925, 0, ["Terragroup"], ["Bot"], "BotZoneMain");
+        CoreMod.AddNewSpawnPoint("Spawn7", "laboratory", -260.9297, 4.117605, -366.114, 0, ["Terragroup"], ["Bot"], "BotZoneMain");
+        CoreMod.AddNewSpawnPoint("Spawn8", "laboratory", -251.4255, 4.098026, -318.5574, 0, ["Terragroup"], ["Bot"], "BotZoneMain");
+        CoreMod.AddNewSpawnPoint("Spawn9", "laboratory", -258.5861, 4.098026, -318.506, 0, ["Terragroup"], ["Bot"], "BotZoneMain");
+    }
+
+    static DatabaseInitialization()
+    {
+        DatabaseInit.createTemplates();
+        DatabaseInit.applyGlobalsChanges();
+        DatabaseInit.createTranslations();
+        DatabaseInit.applyMapsChanges();
+    }
+
+}
+
+
+module.exports = DatabaseInit;
